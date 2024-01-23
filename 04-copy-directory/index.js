1fc2560a26301b10b238b26c21d1f4aaa17a889d
Luba Hadunova <luba.godunova2018@gmail.com>
2024-01-23 10:50:23 +0100

    fix: change 04-copy-directory solution to promises

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,25 +4,30 @@ const pathToFolder = path.join(__dirname, 'files');
 const pathToFolderCopy = path.join(__dirname, 'files-copy');
 
 function copyDir(pathFrom, pathTo) {
-  fs.mkdir(pathTo, { recursive: true }, () => {});
-  fs.readdir(pathTo, (err, files) => {
-    for (let i = 0; i < files.length; i += 1) {
-      const pathToFile = path.join(pathTo, files[i]);
-      fs.unlink(pathToFile, () => {});
-    }
-  });
-  fs.readdir(pathFrom, (err, files) => {
-    for (let i = 0; i < files.length; i += 1) {
-      const pathToFile = path.join(pathFrom, files[i]);
-      const pathToFileCopy = path.join(pathTo, files[i]);
-      fs.copyFile(
-        pathToFile,
-        pathToFileCopy,
-        fs.constants.COPYFILE_FICLONE,
-        () => {},
-      );
-    }
-  });
+  fs.promises
+    .mkdir(pathTo, { recursive: true }, () => {})
+    .then(() => {
+      fs.promises.readdir(pathTo).then((files) => {
+        for (let i = 0; i < files.length; i += 1) {
+          const pathToFile = path.join(pathTo, files[i]);
+          fs.unlink(pathToFile, () => {});
+        }
+      });
+    })
+    .then(() => {
+      fs.promises.readdir(pathFrom).then((files) => {
+        for (let i = 0; i < files.length; i += 1) {
+          const pathToFile = path.join(pathFrom, files[i]);
+          const pathToFileCopy = path.join(pathTo, files[i]);
+          fs.copyFile(
+            pathToFile,
+            pathToFileCopy,
+            fs.constants.COPYFILE_FICLONE,
+            () => {},
+          );
+        }
+      });
+    });
 }
 
 copyDir(pathToFolder, pathToFolderCopy);
