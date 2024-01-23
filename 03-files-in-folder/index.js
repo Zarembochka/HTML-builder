862b92d62d778fc4e533beb76d54dd245cd3d605
Luba Hadunova <luba.godunova2018@gmail.com>
2024-01-23 08:28:33 +0100

    fix: remove information about files in subfolders

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,10 +5,10 @@ const pathToFolder = path.join(__dirname, 'secret-folder');
 function readFilesInFolder(pathToFolder) {
   fs.readdir(pathToFolder, { withFileTypes: true }, (err, files) => {
     for (let i = 0; i < files.length; i += 1) {
-      if (files[i].isDirectory()) {
-        const pathToDirectoryInFolder = path.join(pathToFolder, files[i].name);
-        readFilesInFolder(pathToDirectoryInFolder);
-      }
+      // if (files[i].isDirectory()) {
+      //   const pathToDirectoryInFolder = path.join(pathToFolder, files[i].name);
+      //   readFilesInFolder(pathToDirectoryInFolder);
+      // }
       if (files[i].isFile()) {
         const pathToFile = path.join(pathToFolder, files[i].name);
         const fileObject = path.parse(pathToFile);
